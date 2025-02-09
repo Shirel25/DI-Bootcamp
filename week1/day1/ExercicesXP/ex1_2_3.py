f76337abff2d07e7af7bf391d65a22e885177053
Shirel25 <amoziegshirel@gmail.com>
2025-02-09 20:32:44 +0100

    exercices1_2_3

---
+++ week1/day1/ExercicesXP/ex1_2_3.py
@@ -0,0 +1,13 @@
+#exercice1
+for i in range (4) : print("Hello world")
+
+#exercice2
+result = (99^3)*8
+print(result)
+
+#exercice3
+5 < 3 #False
+3 == 3 #True
+3 == "3" #False
+#"3" > 3 #Error
+"Hello" == "hello" #False
