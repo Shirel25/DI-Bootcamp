b94b127bdae824d4d4007c42e32a8f1f363cd371
Shirel25 <amoziegshirel@gmail.com>
2025-02-09 21:06:17 +0100

    exercice8

---
+++ week1/day1/ExercicesXP/ex8.py
@@ -0,0 +1,6 @@
+user_name = input("What is your name ?")
+
+if user_name == "Shirel":
+    print("OMG such a beautiful name !")
+else:
+    print("Well, how do you deal with that name every day ??")
