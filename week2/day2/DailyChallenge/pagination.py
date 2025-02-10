237a9e9b2822bf8e8f043e1763e1a59474c0379e
Shirel25 <amoziegshirel@gmail.com>
2025-02-10 20:52:53 +0100

    dailyChallenge

---
+++ week2/day2/DailyChallenge/pagination.py
@@ -0,0 +1,65 @@
+class Pagination:
+    def __init__(self, items = None, pageSize = 10):
+        self.items = items if items is not None else []
+        self.pageSize = int(pageSize)
+        self.currentPage = 1
+        # len(the number of items) + len(the number of pages)-1 in the case the items don't fill completely the last page 
+        self.totalPages = (len(self.items) + self.pageSize - 1) // self.pageSize
+
+    def getVisibleItems(self):
+        start = (self.currentPage-1) * self.pageSize   # lists start at 0 and currentPage = 1
+        end = start + self.pageSize
+        visibleItems = self.items[start:end]
+        print(visibleItems)
+        return visibleItems
+    
+    def prevPage(self):
+        if self.currentPage > 1 :
+            self.currentPage -= 1
+            self.getVisibleItems() 
+        else:
+            print("You're already on the first page.")
+
+    def nextPage(self):
+        if self.currentPage < self.totalPages :
+            self.currentPage += 1
+            self.getVisibleItems() 
+        else:
+            print("You're already on the last page.")
+
+    def firstPage(self):
+        self.currentPage = 1
+        self.getVisibleItems() 
+
+    def lastPage(self):
+        self.currentPage = self.totalPages
+        self.getVisibleItems() 
+
+    def goToPage(self, pageNum):
+        if pageNum < 1:
+            self.firstPage()
+        elif pageNum > self.totalPages:
+            self.lastPage()
+        else:
+            self.currentPage = pageNum
+            self.getVisibleItems() 
+    
+            
+
+alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+
+p = Pagination(alphabetList, 3)
+p.getVisibleItems() # ['a', 'b', 'c']
+
+p.prevPage() # You're already on the first page.
+p.nextPage() # ['d', 'e', 'f']
+
+p.firstPage() #['a', 'b', 'c']
+p.lastPage() # ['y', 'z']
+
+p.goToPage(6) #['p', 'q', 'r']
+
+print("closest to first page")
+p.goToPage(-3) # ['a', 'b', 'c']
+print("closest to last page")
+p.goToPage(100) # ['y', 'z']
